457e49da3c6d47b84f5c33a646c77b435450ab1e
Lwazi Shangase <lwazoshuku@gmail.com>
2023-10-05 19:20:57 -0700

    5-printf.c 1st commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,3 +1,13 @@
 #include <stdio.h>
-/* main - Prints message */
-int main(void){ puts("\"Programming is like building a multilingual puzzle");return (0);}
+
+/**
+ * main - Prints message
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * File: 5-printf.c
+ * Auth: Lwazi Shangase
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints message 
+ *                
+ * Return: Always 0.
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
