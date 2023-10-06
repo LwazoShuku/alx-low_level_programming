88d1796a8164fa3bf7a22559a54425df087012f3
Lwazi Shangase <lwazoshuku@gmail.com>
2023-10-06 10:06:52 -0700

    8-print_base16 4th commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -14,11 +14,12 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 16; num++){
+	for (num = 0; num < 16; num++)
+	{
 		putchar(num % 16 < 10 ? num % 16 + '0' : num % 16 - 10 + 'a');
 	}
 
-    	putchar('\n');
+	putchar('\n');
 
 	return (0);
 }
