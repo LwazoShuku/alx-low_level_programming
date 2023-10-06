8457a28ba2f29f25870aaed0aeb46589814b5513
Lwazi Shangase <lwazoshuku@gmail.com>
2023-10-06 09:43:56 -0700

    8-print_base16 1st commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+/*
+ * File: 8-print_base16.c
+ * Auth: Lwazi Shangase
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints all the numbers of base 16 in lowercase.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int num;
+	for (num = 0; num < 16; num++) 
+        	putchar(num % 16 < 10 ? num % 16 + '0' : num % 16 - 10 + 'a');
+    	
+
+    	putchar('\n');
+
+	return (0);
+}
