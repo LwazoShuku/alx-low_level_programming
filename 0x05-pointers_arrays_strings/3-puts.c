13b0db1e231c8f5382b34cbbbb576308dabde7a6
Lwazi Shangase <lwazoshuku@gmail.com>
2023-10-17 06:21:56 -0700

    3-puts.c 3rd commit

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -9,10 +9,10 @@
  * _puts - Prints a string to stdout.
  * @str: The string to be printed.
  */
-void _puts(char *str)
-{
-	while (*str)
-		_putchar(*str++);
-
-	_putchar('\n');
+void _puts(char *str) {
+    while (*str != '\0') {
+        _putchar(*str);
+        str++;
+    }
+    _putchar('\n');
 }
