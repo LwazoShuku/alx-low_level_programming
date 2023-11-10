1c97716636253c5ab797a67f84e8e28ae0e94a0d
Lwazi Shangase <lwazoshuku@gmail.com>
2023-11-10 06:41:34 -0800

    2nd commit

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+/*
+ * File: 0-print_name.c
+ * Auth: Lwazi Shangase
+ */
+
+#include <stdio.h>
+
+/**
+ * print_name - Prints a name.
+ * @name: The name to be printed.
+ * @f: A pointer to a function that prints a name.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
