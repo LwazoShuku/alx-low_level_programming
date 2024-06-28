8e860868927fb3f887076644bd6eada717c45e6d
Lwazi Shangase <43201321+LwazoShuku@users.noreply.github.com>
2024-06-28 02:36:40 +0200

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,20 +5,11 @@
  * File: main.h
  * Auth: Lwazi Shangase
  * Desc: Header file containing prototypes for all functions
- *       used in the 0x03-more_functions_nested_loops directory.
+ *       used in the 0x03-debugging directory.
  */
 
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
-void print_numbers(void);
-void print_most_numbers(void);
-void more_numbers(void);
-void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
-void print_number(int n);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
